75f92628cce04ee1f1669aa2d79d98b1d58c64e5
Aakash-Sleur <dsaakash463@gmail.com>
2026-01-17 23:05:55 +0530

    first commit

---
+++ queue-demo/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bullmq';
+import { EmailModule } from './email/email.module';
+import configuration from './config/configuration';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        connection: {
+          host: configService.get('redis.host'),
+          port: configService.get('redis.port'),
+        },
+      }),
+    }),
+    EmailModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ queue-demo/src/email/email.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { BullModule } from '@nestjs/bullmq';
+import { EmailProcessor } from './email.processor';
+import { EmailController } from './email.controller';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    BullModule.registerQueue({
+      name: "email"
+    })
+  ],
+  providers: [EmailService, EmailProcessor, ConfigService],
+  controllers: [EmailController]
+})
+export class EmailModule {}
